39d5429fee93c2e883c35db176c3bc4df78ee4dc
mananbordia <45929094+mananbordia@users.noreply.github.com>
2020-06-05 01:48:50 +0530

    Code to get interest rate of return

---
+++ Get_IRR.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+double func(double x){
+    return 50/x + 25/(x*x) + 45/(x*x*x) + 70/(x*x*x*x); 
+}
+int main(){
+    double l = 0, r=1;
+    while(l<r){
+        double m = (l+r)/2;
+        if(func(m+1)>=100 && func(m+1)-100<1e-5){
+            r = m;
+            break;
+        }
+        if(func(m+1)>100){
+            l = m;
+        }
+        else r = m;
+    }
+    cout<<r; // 29.0128
+}
